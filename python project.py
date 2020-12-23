e9f452d526030530f83c7d2c3e22ca3839fbe8c2
ChhaviGautam <76486970+ChhaviGautam@users.noreply.github.com>
2020-12-23 10:23:44 +0530

    Update python project.py

--- python project.py
+++ python project.py
@@ -1,12 +1,12 @@
-import mysql.connector
-from tkinter import *
-from tkinter import messagebox
+import mysql.connector                                     #to view saved data
+from tkinter import *                                      #for creating GUI application
+from tkinter import messagebox                             #for showing message after insertion 
 db=mysql.connector.connect(host='localhost',
-                           user='root',
+                           user='root',                    #for creating connectivity
                            password='root',
                            database='hospital'
                            )
-my_cur=db.cursor()
+my_cur=db.cursor()                                          #for creating changes one row at a time
 
 def entry():
     root=Tk()
@@ -22,10 +22,10 @@ def entry():
     Label(root,text="").grid(row=7,column=0)
     Label(root,text="doctor",bg="pink").grid(row=8,column=0)
     Label(root,text="").grid(row=9,column=0)
-    Label(root,text="address",bg="yellow").grid(row=10,column=0)
+    Label(root,text="address",bg="yellow").grid(row=10,column=0)                        #function about hows the form looks like
     Label(root,text="").grid(row=11,column=0)
-    Label(root,text="mob",bg="pink").grid(row=12,column=0)
-    Label(root,text="").grid(row=13,column=0)
+    Label(root,text="mob",bg="pink").grid(row=12,column=0)                              #using label for making line widget
+    Label(root,text="").grid(row=13,column=0)                                           #using root for adding object
     Label(root,text="testreport",bg="red").grid(row=14,column=0)
     Label(root,text="").grid(row=15,column=0)
     Label(root,text="admitteddays",bg="yellow").grid(row=16,column=0)
@@ -36,7 +36,7 @@ def entry():
     v2=StringVar()
     v3=StringVar()
     v4=StringVar()
-    v5=StringVar()
+    v5=StringVar()                                                                  #entries for filling this later
     v6=StringVar()
     v7=StringVar()
     v8=StringVar()
@@ -44,7 +44,7 @@ def entry():
     e1=Entry(root,textvariable=v1).grid(row=2,column=1)
     e2=Entry(root,textvariable=v2).grid(row=4,column=1)
     e3=Entry(root,textvariable=v3).grid(row=6,column=1)
-    e4=Entry(root,textvariable=v4).grid(row=8,column=1)
+    e4=Entry(root,textvariable=v4).grid(row=8,column=1)                             #entry widget is used to create input field
     e5=Entry(root,textvariable=v5).grid(row=10,column=1)
     e6=Entry(root,textvariable=v6).grid(row=12,column=1)
     e7=Entry(root,textvariable=v7).grid(row=14,column=1)
@@ -57,16 +57,16 @@ def entry():
         name=v2.get()
         age=v3.get()
         doctor=v4.get()
-        address=v5.get()
+        address=v5.get()                                                          #function for inserting all entries
         mob=v6.get()
         testreport=v7.get()
         admitteddays=v8.get()
         amount=v9.get()
         
-        my_cur=db.cursor()
-        my_cur.execute('insert into reports values(%s,%s,%s,%s,%s,%s,%s,%s,%s)',(serialno,name,age,doctor,address,mob,testreport,admitteddays,amount))
-        db.commit()
-        messagebox.showinfo('Wow','1 record inserted')
+        my_cur=db.cursor()                                                #creating database
+        my_cur.execute('insert into reports values(%s,%s,%s,%s,%s,%s,%s,%s,%s)',(serialno,name,age,doctor,address,mob,testreport,admitteddays,amount))     #showing the final database
+        db.commit()                                                         #it execute many entries at one time
+        messagebox.showinfo('Wow','1 record inserted')                      #message shows after insertion
         v1.set('')
         v2.set('')
         v3.set('')
@@ -80,7 +80,7 @@ def entry():
     def clear():
         v1.set('')
         v2.set('')
-        v3.set('')
+        v3.set('')                                                       #for reseting clear all entries
         v4.set('')
         v5.set('')
         v6.set('')
@@ -89,9 +89,9 @@ def entry():
         v9.set('')
 
     def close():
-        root.destroy()
+        root.destroy()                                                        #for exit
     Button(root,text='SUBMIT',command=insert).grid(row=21,column=0)
-    Button(root,text='RESET',command=clear).grid(row=21,column=1)
+    Button(root,text='RESET',command=clear).grid(row=21,column=1)               #creating buttons
     Button(root,text='EXIT',command=close).grid(row=21,column=2)
     root.mainloop()
 
